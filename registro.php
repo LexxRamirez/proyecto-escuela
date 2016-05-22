4ac77409f908fdb7f95e02b2915cc1672124d4f5
LexxRamirez <antoni.the21@gmail.com>
2016-05-21 22:37:31 -0600

    Principio

---
+++ registro.php
@@ -0,0 +1,349 @@
+<!DOCTYPE html>
+<html>
+<title>..::C.E</title>
+<body>
+	<div id="contenedor-sup" style="width:900px; margin: auto;">
+	<h3>FICHA DE MATRICULA</h3>
+	<h5>Centro Escolar Canton "La Canoa". Codigo: 13072 Distrito: 12-05</h5>
+	<table>
+		<tr>
+			<td>
+				<label>Año:</label>
+			</td>
+			<td>
+				<input type="text" name="anho">
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<label>Grado:</label>
+			</td>
+			<td>
+				<input type="text" name="grado">
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<label>Alumno numero:</label>
+			</td>
+			<td>
+				<input type="text" name="alumN">
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<label>Matricula numero:</label>
+			</td>
+			<td>
+				<input type="text" name="matriculaN">
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<label>Nombre del alumno(a):</label>
+			</td>
+			<td>
+				<input type="text" name="nombre">
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<label>NIE</label>
+			</td>
+			<td>
+				<input type="text" name="nie">
+			</td>
+		</tr>
+	</table>
+	<table>
+		<tr>
+			<td>
+				<label>Repite grado:</label>
+			</td>
+			<td>
+				<input type="text" name="repite" size="7px">
+			</td>
+			<td>
+				<label>Estudio parbularia:</label>
+			</td>
+			<td>
+				<input type="text" name="parbularia" size="7px">
+			</td>
+			<td>
+				<label>Año en el que estudio el ultimo grado:</label>
+			</td>
+			<td>
+				<input type="text" name="ultimo" size="7px">
+			</td>
+		</tr>
+	</table>
+	<table>
+		<tr>
+			<td style="width:200px">
+				<label >Municipio en el que esudio el ultimo año:</label>
+			</td>
+			<td>
+				<input type="text" name="Muniultimo">
+			</td>
+			<td>
+				<label>Codigo:</label>
+			</td>
+			<td>
+				<input type="text" name="codigo">
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<label>Tipo de discapacidad:</label>
+			</td>
+			<td>
+				<input type="text" name="discapacidad">
+			</td>
+			<td>
+				<label>Tipo de educacion especial:</label>
+			</td>
+			<td>
+				<input type="text" name="eduEspecial">
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<label>Lugar de residencia:</label>
+			</td>
+			<td>
+				<input type="checkbox" name="chek1">
+				<label>Rural</label>
+				<input type="checkbox" name="chek2">
+				<label>Urbana</label>
+			</td>
+		</tr>
+	</table>	
+	<h5>Municipio en el que estudia actualmente: <u><i>San Miguel</i></u></h5>
+	<h5>Codigo: 1217</h5>
+	<table>
+		<tr>
+			<td>
+				<label>Fecha de nacimiento:</label>
+			</td>
+			<td>
+				<input type="text" name="fecha">
+			</td>
+			<td>
+				<label>Edad:</label>
+			</td>
+			<td>
+				<input type="text" name="edad">
+			</td>
+			<td>
+				<label>Tiene sobre edad:</label>
+			</td>
+			<td>
+				<input type="checkbox" name="cheque1">
+				<label>SI</label>
+				<input type="checkbox" name="cheque2">
+				<label>NO</label>
+			</td>
+		</tr>
+		<tr>
+			<td >
+				<label>Tipo de trabajo que realiza:</label>
+			</td>
+			<td colspan="5">
+				<input type="text" name="trabajo" size="60px">
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<label>Vive con:</label>
+			</td>
+			<td colspan="6">
+				<input type="checkbox" name="vive1">
+				<label>Mamá</label>
+				<input type="checkbox" name="vive2">
+				<label>Papá</label>
+				<input type="checkbox" name="vive3">
+				<label>Papá y Mamá</label>
+				<input type="checkbox" name="vive4">
+				<label>Familiar</label>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<label>Tipo de enfermedad:</label>
+			</td>
+			<td>
+				<input type="text" name="enfermedad">
+			</td>
+			<td colspan="2">
+				<label>Vacunas completas:</label>
+				<input type="checkbox" name="cheque1">
+				<label>SI</label>
+				<input type="checkbox" name="cheque2">
+				<label>NO</label>
+			</td>
+		</tr>
+		<tr>
+			<td colspan="6">
+				<label>Nombre del padre o encargado:</label>
+				<input type="text" name="encargado" size="60px">
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<label>DUI:</label>
+			</td>
+			<td>
+				<input type="text" name="dui">
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<label>Tel de casa:</label>
+			</td>
+			<td>
+				<input type="text" name="Tcasa">
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<label>Tel de celular:</label>
+			</td>
+			<td>
+				<input type="text" name="Tcel">
+			</td>
+		</tr>
+	</table>
+	<h4 style="text-align:center">SITUACION SOCIOECONOMICA</h4>
+	<fieldset>
+		<table>
+			<tr>
+				<td>
+					<label>INGRESO FAMILIAR:</label>
+				</td>
+				<td>
+					<input type="checkbox" name="ch1">
+					<label>PADRE</label>
+					<input type="checkbox" name="ch2">
+					<label>MADRE</label>
+					<input type="checkbox" name="ch3">
+					<label>OTROS</label>
+					<input type="text" name="cheque2">
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<label>VIVIENDA:</label>
+				</td>
+				<td>
+					<input type="checkbox" name="ch1">
+					<label>PROPIO</label>
+					<input type="checkbox" name="ch2">
+					<label>ALQUILADA</label>
+					<input type="checkbox" name="ch3">
+					<label>TIPO DE VIVIENDA</label>
+					<input type="text" name="cheque2">
+				</td>
+			</tr>
+		</table>
+	</fieldset>
+	<h4 style="text-align:center">DATOS PERSONALES DEL NIÑO Y GRUPO FAMILIAR</h4>
+	<fieldset>
+		<table>
+			<tr>
+				<td>
+					<label>Embarazo:</label>
+					<input type="checkbox" name="ch1">
+					<label>Normal</label>
+					<input type="checkbox" name="ch1">
+					<label>Anormal</label>
+				</td>
+				<td>
+					<label>Parto:</label>
+					<input type="checkbox" name="ch1">
+					<label>Normal</label>
+					<input type="checkbox" name="ch1">
+					<label>Anormal</label>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<label>N. de la madre:</label>
+					<input type="text" name="nmadre">
+				</td>
+				<td>
+					<label>Edad:</label>
+					<input type="text" name="Medad" size="5px">
+					<label>Sabe:</label>
+					<input type="checkbox" name="ch1">
+					<label>Leer</label>
+					<input type="checkbox" name="ch1">
+					<label>Escribir</label>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<label>N. del padre:</label>
+					<input type="text" name="npadre">
+				</td>
+				<td>
+					<label>Edad:</label>
+					<input type="text" name="Pedad" size="5px">
+					<label>Sabe:</label>
+					<input type="checkbox" name="ch1">
+					<label>Leer</label>
+					<input type="checkbox" name="ch1">
+					<label>Escribir</label>
+				</td>
+			</tr>
+			<tr>
+				<td colspan="2">
+					<label>N° de hermanos:</label>
+					<input type="text" name="Pedad" size="5px">
+					<label>N° de hermanas:</label>
+					<input type="text" name="Pedad" size="5px">
+				</td>
+			</tr>
+			<tr>
+				<td colspan="2">
+					<label>Comportamiento y personalidad del niño:</label>
+				</td>
+			</tr>
+			<tr>
+				<td colspan="4">
+					<input type="checkbox" name="ch1">
+					<label>Inquieto</label>
+					<input type="checkbox" name="ch1">
+					<label>Timido</label>
+					<input type="checkbox" name="ch1">
+					<label>Cooperativo</label>
+					<input type="checkbox" name="ch1">
+					<label>Respetuoso</label>
+					<input type="checkbox" name="ch1">
+					<label>Responsable</label>
+					<input type="checkbox" name="ch1">
+					<label>Creativo</label>
+					<input type="checkbox" name="ch1">
+					<label>Ordenado</label>
+				</td>
+			</tr>
+		</table>
+	</fieldset>
+	<h4 style="text-align:center">DOCUMENTO QUE PRESENTO</h4>
+	<table>
+		<tr>
+				<td colspan="4">
+					<input type="checkbox" name="ch1">
+					<label>Copia de partida</label>
+					<input type="checkbox" name="ch1">
+					<label>Certificado Original</label>
+					<input type="checkbox" name="ch1">
+					<label>DUI</label>
+					<input type="checkbox" name="ch1">
+					<label>Constancia de conducta</label>
+				</td>
+			</tr>
+	</table>
+	</div>
+	<br>
+</body>
+</html>
