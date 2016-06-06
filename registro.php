3e81b3ec4684deff789027479c1de83a5aab46ae
LexxRamirez <antoni.the21@gmail.com>
2016-06-05 19:39:28 -0600

    Modificacion de formulario

--- registro.php
+++ registro.php
@@ -29,7 +29,7 @@
     <h5><label>Centro Escolar Canton "La Canoa". Codigo: 13072 Distrito: 12-05</label></h5>
     <table >
         <tr>
-            <td>
+            <td width="200px">
                 <label>Año:</label>
             </td>
             <td>
@@ -92,6 +92,7 @@
             <td>
                 <input type="text" name="parbularia" size="12px" class="form-control">
             </td>
+        <tr>
             <td>
                 <label>Año en el que estudio el ultimo grado:</label>
             </td>
@@ -99,9 +100,8 @@
                 <input type="text" name="ultimo" size="12px" class="form-control">
             </td>
         </tr>
-    </table>
-    <br>
-    <table>
+            
+        </tr>
         <tr>
             <td style="width:200px">
                 <label >Municipio en el que esudio el ultimo año:</label>
@@ -131,130 +131,172 @@
             </td>
         </tr>
         <tr><td><br></td></tr>
+        
+    </table>  
+    <table>
         <tr>
-            <td>
+            <td style=" width:200px">
                 <label>Lugar de residencia:</label>
             </td>
-            <td>
+            <td style=" width:100px">
                 <input type="checkbox" name="chek1" class="checkbox">
                 <label>Rural</label>
-                <input type="checkbox" name="chek2" class="checkbox">
-                <label>Urbana</label>
+                
             </td>
+            <td colspan="2"><input type="checkbox" name="chek2" class="checkbox">
+                <label>Urbana</label></td>
         </tr>
-    </table>    
+    </table>  
     <h5><label>Municipio en el que estudia actualmente: <u><i>San Miguel</i></u></label></h5>
     <h5><label>Codigo: 1217</label></h5>
     <table>
         <tr>
-            <td>
+            <td width="200px">
                 <label>Fecha de nacimiento:</label>
             </td>
             <td>
-                <input type="text" name="fecha" class="form-control" size="30px">
+                <input type="text" name="fecha" class="form-control" size="36px">
             </td>
             <td>
                 <label>Edad:</label>
             </td>
             <td>
-                <input type="text" name="edad" class="form-control">
+                <input type="text" name="edad" class="form-control" size="37px">
             </td>
-            <td>
+        </tr>
+    </table>
+    <table>
+        <tr>
+            <td style=" width:200px">
                 <label>Tiene sobre edad:</label>
             </td>
-            <td>
+            <td style=" width:75px" colspan="2">
                 <input type="checkbox" name="cheque1">
                 <label>SI</label>
+                
+            </td>
+            <td>
                 <input type="checkbox" name="cheque2">
                 <label>NO</label>
             </td>
         </tr>
+    </table>
+    <table>
         <tr>
-            <td >
+            <td width="200pxs">
                 <label>Tipo de trabajo que realiza:</label>
             </td>
-            <td colspan="5">
-                <input type="text" name="trabajo" size="60px" class="form-control">
+            <td>
+                <input type="text" name="trabajo" size="100%" class="form-control">
             </td>
         </tr>
+    </table>
+    <table>
         <tr>
-            <td>
+            <td width="200px">
                 <label>Vive con:</label>
             </td>
-            <td colspan="6">
+            <td width="100px">
                 <input type="checkbox" name="vive1">
                 <label>Mamá</label>
+            </td>
+            <td width="100px">
                 <input type="checkbox" name="vive2">
                 <label>Papá</label>
+            </td>
+            <td width="150px">
                 <input type="checkbox" name="vive3">
                 <label>Papá y Mamá</label>
-                <input type="checkbox" name="vive4">
+            </td>
+            <td>
+                 <input type="checkbox" name="vive4">
                 <label>Familiar</label>
             </td>
         </tr>
+    </table>
+    <table>
         <tr>
-            <td>
+            <td width="200px">
                 <label>Tipo de enfermedad:</label>
             </td>
             <td>
-                <input type="text" name="enfermedad" class="form-control">
+                <input type="text" name="enfermedad" class="form-control" size="100%">
             </td>
-            <td colspan="2">
+        </tr>
+    </table>
+    <table>
+        <tr>
+            <td width="200px;">
                 <label>Vacunas completas:</label>
+            </td>
+            <td width="75px;">
                 <input type="checkbox" name="cheque1">
                 <label>SI</label>
+            </td>
+            <td>
                 <input type="checkbox" name="cheque2">
                 <label>NO</label>
             </td>
         </tr>
+    </table>
+    <table>
         <tr>
-            <td>
+            <td width="200px">
                 <label>Nombre del padre o encargado:</label>
                 
             </td>
-            <td colspan="5"><input type="text" name="encargado" size="100%" class="form-control"></td>
-        </tr>
-        <tr>
             <td>
+                <input type="text" name="encargado" class="form-control" size="100%">
+            </td>
+        </tr>
+    </table>
+    <table>
+        <tr>
+            <td width="200px">
                 <label>DUI:</label>
             </td>
-            <td colspan="2">
-                <input type="text" name="dui" size="50%" class="form-control">
+            <td>
+                <input type="text" name="dui" size="36px;" class="form-control">
             </td>
         </tr>
         <tr>
             <td>
                 <label>Tel de casa:</label>
             </td>
-            <td colspan="2">
-                <input type="text" name="Tcasa" size="50%" class="form-control">
+            <td>
+                <input type="text" name="Tcasa" size="36px" class="form-control">
             </td>
         </tr>
         <tr>
             <td>
                 <label>Tel de celular:</label>
             </td>
-            <td colspan="2">
-                <input type="text" name="Tcel" size="50%" class="form-control">
+            <td>
+                <input type="text" name="Tcel" size="36px" class="form-control">
             </td>
         </tr>
     </table>
+    <br>
     <h4 style="text-align:center">SITUACION SOCIOECONOMICA</h4>
     <fieldset>
         <table>
             <tr>
-                <td>
+                <td width="200px">
                     <label>INGRESO FAMILIAR:</label>
                 </td>
-                <td>
+                <td width="100px">
                     <input type="checkbox" name="ch1">
                     <label>PADRE</label>
+                </td>
+                <td width="200px">
                     <input type="checkbox" name="ch2">
                     <label>MADRE</label>
-                    <input type="checkbox" name="ch3">
                 </td>
-                <td><label>OTROS</label></td>
-                <td><input type="text" name="otr" class="form-control"></td>
+                <td>
+                    <label>OTROS:</label></td>
+                <td>
+                    <input type="text" name="otr" class="form-control" size="40px">
+                </td>
             </tr>
             <tr>
                 <td>
@@ -263,14 +305,16 @@
                 <td>
                     <input type="checkbox" name="ch1">
                     <label>PROPIO</label>
+                </td>
+                <td>
                     <input type="checkbox" name="ch2">
                     <label>ALQUILADA</label>
-                    <input type="checkbox" name="ch3">
-                    
-                    
                 </td>
-                <td><label>TIPO DE VIVIENDA</label></td>
-                <td><input type="text" name="cheque2" class="form-control"></td>
+                <td>
+                    <label>TIPO DE VIVIENDA:</label></td>
+                <td>
+                    <input type="text" name="cheque2" class="form-control">
+                </td>
             </tr>
         </table>
     </fieldset>
@@ -278,93 +322,136 @@
     <fieldset>
         <table>
             <tr>
-                <td colspan="3">
+                <td width="200px">
                     <label>Embarazo:</label>
+                </td>
+                <td width="100px">
                     <input type="checkbox" name="ch1">
                     <label>Normal</label>
+                </td>
+                <td width="200px">
                     <input type="checkbox" name="ch1">
                     <label>Anormal</label>
                 </td>
-                <td>
+                <td width="150px">
                     <label>Parto:</label>
+                </td>
+                <td width="100px">
                     <input type="checkbox" name="ch1">
                     <label>Normal</label>
+                </td>
+                <td>
                     <input type="checkbox" name="ch1">
                     <label>Anormal</label>
                 </td>
             </tr>
+        </table>
+        <table>
             <tr>
-                <td>
+                <td width="200px">
                     <label>N. de la madre:</label>
                 </td>
-                <td><input type="text" name="nmadre" class="form-control"></td>
+                <td>
+                    <input type="text" name="nmadre" class="form-control" size="36px">
+                </td>
                 <td>
                     <label>Edad:</label>
-                    
                 </td>
-                <td><input type="text" name="Medad" size="5px" class="form-control"></td>
                 <td>
+                    <input type="text" name="Medad" class="form-control" size="37px">
+                </td>
+            </tr>
+        </table>
+        <table>
+            <tr>
+                <td width="200px">
                     <label>Sabe:</label>
+                </td>
+                <td width="100px">
                     <input type="checkbox" name="ch1">
                     <label>Leer</label>
+                </td>
+                <td>
                     <input type="checkbox" name="ch1">
                     <label>Escribir</label>
                 </td>
             </tr>
+        </table>
+        <table>
             <tr>
-                <td>
+                <td width="200px">
                     <label>N. del padre:</label>
                 </td>
                 <td>
-                    <input type="text" name="npadre" class="form-control">
+                    <input type="text" name="nmadre" class="form-control" size="36px">
                 </td>
                 <td>
                     <label>Edad:</label>
                 </td>
                 <td>
-                    <input type="text" name="Pedad" size="5px" class="form-control">
+                    <input type="text" name="Medad" class="form-control" size="37px">
                 </td>
-                <td>
+            </tr>
+        </table>
+        <table>
+            <tr>
+                <td width="200px">
                     <label>Sabe:</label>
+                </td>
+                <td width="100px">
                     <input type="checkbox" name="ch1">
                     <label>Leer</label>
+                </td>
+                <td>
                     <input type="checkbox" name="ch1">
                     <label>Escribir</label>
                 </td>
             </tr>
+        </table>
+        <table>
             <tr>
-                <td>
+                <td width="200px">
                     <label>N° de hermanos:</label>
                 </td>
                 <td>
-                    <input type="text" name="Pedad" size="5px" class="form-control">
+                    <input type="text" name="Pedad" size="36px" class="form-control">
                 </td>
                 <td>
                     <label>N° de hermanas:</label>
                 </td>
                 <td>
-                    <input type="text" name="Pedad" size="5px" class="form-control">
+                    <input type="text" name="Pedad" size="37px" class="form-control">
                 </td>
             </tr>
+        </table>
+        <h4 style="text-align:center">COMPORTAMIENTO DEL NIÑO</h4>
+        <table style="margin:auto;">
             <tr>
-                <td colspan="2">
-                    <label>Comportamiento y personalidad del niño:</label>
-                </td>
-            </tr>
-            <tr>
-                <td colspan="4">
+                <td width="100px">
                     <input type="checkbox" name="ch1">
                     <label>Inquieto</label>
+                </td>
+                <td width="100px">
                     <input type="checkbox" name="ch1">
                     <label>Timido</label>
+                </td>
+                <td width="120px">
                     <input type="checkbox" name="ch1">
                     <label>Cooperativo</label>
+                </td>
+                <td width="120px">
                     <input type="checkbox" name="ch1">
                     <label>Respetuoso</label>
+                </td>
+                <td width="120px">
                     <input type="checkbox" name="ch1">
                     <label>Responsable</label>
+                </td>
+                <td width="100px">
                     <input type="checkbox" name="ch1">
                     <label>Creativo</label>
+                </td>
+                <td width="100px">
                     <input type="checkbox" name="ch1">
                     <label>Ordenado</label>
                 </td>
@@ -372,19 +459,25 @@
         </table>
     </fieldset>
     <h4 style="text-align:center">DOCUMENTO QUE PRESENTO</h4>
-    <table>
+    <table style="margin:auto">
         <tr>
-                <td colspan="4">
-                    <input type="checkbox" name="ch1">
-                    <label>Copia de partida</label>
-                    <input type="checkbox" name="ch1">
-                    <label>Certificado Original</label>
-                    <input type="checkbox" name="ch1">
-                    <label>DUI</label>
-                    <input type="checkbox" name="ch1">
-                    <label>Constancia de conducta</label>
-                </td>
-            </tr>
+            <td width="150px">
+                <input type="checkbox" name="ch1">
+                <label>Copia de partida</label>  
+            </td>
+            <td width="200px">
+                <input type="checkbox" name="ch1">
+                <label>Certificado Original</label>
+            </td>
+            <td width="100px">
+                <input type="checkbox" name="ch1">
+                <label>DUI</label>
+            </td>
+            <td width="px">
+                <input type="checkbox" name="ch1">
+                <label>Constancia de conducta</label>
+            </td>
+        </tr>
     </table>
     </div>
     <br>
